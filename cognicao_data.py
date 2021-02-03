5862ad78384e0335e3a4d5aa963d8ed77769225b
mzstrelow <65195299+mzstrelow@users.noreply.github.com>
2021-02-03 19:57:39 -0300

    Add files via upload

---
+++ cognicao_data.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 19 18:14:25 2020
+
+@author: mzs_9
+"""
+
+import numpy as np
+from numpy import int64
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data_prep = pd.read_csv(r'C:\Users\mzs_9\OneDrive\Área de Trabalho\Cognição e PD\data.csv')
